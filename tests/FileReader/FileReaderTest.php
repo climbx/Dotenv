413f1b41370d51bf423a9de30c9f52f1672a227f
Pierre Gaimard <pierre@gaimard-family.fr>
2021-05-25 13:11:27 +0200

    Add FileReaderTest test class

---
+++ tests/FileReader/FileReaderTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Climbx\Tests\Dotenv\FileReader;
+
+use Climbx\Dotenv\FileReader\FileErrorException;
+use Climbx\Dotenv\FileReader\FileReader;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \Climbx\Dotenv\FileReader\FileReader
+ */
+class FileReaderTest extends TestCase
+{
+    public function testBadFilePath()
+    {
+        $this->expectException(FileErrorException::class);
+        $this->expectExceptionMessage('The .env file do not exists or is not readable. File path: "/path/to/my/.env"');
+
+        $fileReader = new FileReader();
+        $fileReader->getContentAsArray('/path/to/my/.env');
+    }
+
+    public function testGoodFilePath()
+    {
+        $fileReader = new FileReader();
+        $fileContent = $fileReader->getContentAsArray(__FILE__);
+
+        $this->assertIsArray($fileContent);
+    }
+
+    public function testFileContentHasEmptyLines()
+    {
+        $fileReader = new FileReader();
+        $fileContent = $fileReader->getContentAsArray(__FILE__);
+
+        $hasEmptyLine = false;
+        foreach ($fileContent as $line) {
+            if ($line === "\n") {
+                $hasEmptyLine = true;
+            }
+        }
+
+        $this->assertTrue($hasEmptyLine);
+    }
+}
